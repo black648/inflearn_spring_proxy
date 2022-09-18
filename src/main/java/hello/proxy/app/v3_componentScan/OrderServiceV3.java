888afd17011f951c012b9ade1f690f226e1f8685
HKL <bastlwalzer@gmail.com>
2022-09-18 21:48:44 +0900

    * 프록시패턴 - 컴포넌트 스캔

---
+++ src/main/java/hello/proxy/app/v3_componentScan/OrderServiceV3.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v3_componentScan;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepositoryV3;
+
+    public OrderServiceV3(OrderRepositoryV3 orderRepositoryV3) {
+        this.orderRepositoryV3 = orderRepositoryV3;
+    }
+
+    public void orderItem(String itemId) {
+        orderRepositoryV3.save(itemId);
+    }
+
+}
