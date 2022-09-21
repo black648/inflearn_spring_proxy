ad5cb0a53e0b7db305778d9cc03ca0be86aad48c
HKL_L <hyunkyu_lee@lotte.net>
2022-09-22 08:54:49 +0900

    * CGLIB 적용    - JDK Dynamic : 인터페이스형태가 있어야 함.    - CGLIB : 클래스 형태로 프록시 생성 가능

---
+++ src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,31 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreateService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib() {
+        ConcreateService target = new ConcreateService();
+
+        // CGLIB는 Enhancer 를 사용해서 프록시를 생성한다.
+        Enhancer enhancer = new Enhancer();
+        // CGLIB는 구체 클래스를 상속 받아서 프록시를 생성할 수 있다. 어떤 구체 클래스를 상속 받을지 지정한다.
+        enhancer.setSuperclass(ConcreateService.class);
+        // 프록시에 적용할 실행 로직을 할당한다.
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        // 프록시를 생성한다. 앞서 설정한 enhancer.setSuperclass 에서 지정한 클래스를 상속 받아서 프록시가 만들어진다.
+        ConcreateService proxy = (ConcreateService) enhancer.create();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+
+}
